d8b9226ac67343c2e2794a53e0fcb85a1f346903
honest1320 <63172797+honest1320@users.noreply.github.com>
2020-06-30 20:42:34 +0300

    Initial Commit

---
+++ pollster/polls/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+from django.conf.urls import include, url
+from django.contrib import admin
+
+
+class PollsConfig(AppConfig):
+    name = 'polls'
---
+++ pollster/polls/urls.py
@@ -0,0 +1,13 @@
+from django.urls import include, path
+from django.conf.urls import include, url
+
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
